5c5e97f3de2ea61a2662c7d0652c3072b2698383
Suraj Kumar <140057403+S30ULFR05T@users.noreply.github.com>
2024-03-10 21:23:23 +0530

    Create 01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144523
+let accountEmail = "kumasuraj@gmail.com"
+var accountPassword = "suraj"
+accountCity = "Delhi"
+
+let accountState;
+
+// accountId = 2 //not allowed
+
+accountEmail = "kuamrsur@yahoo.com"
+accountPassword = "kumar"
+accountCity = "mumbai"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
