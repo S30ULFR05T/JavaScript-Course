bf1ad1df14c59050013244bbb8c6d76fc3499ae1
Suraj Kumar <140057403+S30ULFR05T@users.noreply.github.com>
2024-03-23 00:18:41 +0530

    Update 01_forloop.js

--- 05_iterrations/01_forloop.js
+++ 05_iterrations/01_forloop.js
@@ -25,9 +25,40 @@
 
 // }
 
-for (let i = 0; i <= 10; i++) {
-  console.log(`Outer loop value: ${i}`);
-  for (let j = 0; j <= 10; j++) {
-    console.log(i + "*" + j + " = " + i*j);
+// for (let i = 0; i <= 10; i++) {
+//   console.log(`Outer loop value: ${i}`);
+//   for (let j = 0; j <= 10; j++) {
+//     console.log(i + "*" + j + " = " + i*j);
+//   }
+// }
+
+
+let myArray = ['flash', "batman", "superman"]
+
+for (let index = 0; index < myArray.length; index++) {
+  const element = myArray[index];
+  console.log(element);
+}
+
+// break and continue
+
+for (let index = 1; index < 20; index++) {
+  if (index == 5){
+    console.log("5 is detected...");
+    break
   }
+
+
+  console.log(`Value of i is ${index}`);
+  
+}
+for (let index = 1; index < 20; index++) {
+  if (index == 5){
+    console.log("5 is detected...");
+    continue
+  }
+
+
+  console.log(`Value of i is ${index}`);
+  
 }
