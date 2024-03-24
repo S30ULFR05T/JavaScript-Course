b4ae78e10f056f4d5ee51ab7e162ef5e01327725
Suraj Kumar <140057403+S30ULFR05T@users.noreply.github.com>
2024-03-24 18:49:22 +0530

    Add files via upload

---
+++ 05_iterrations/02_while_dowhile.js
@@ -0,0 +1,27 @@
+// while loop and do while loop
+
+// while (condition) {      // Syntax
+    
+// }
+
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ['flash', 'superman', 'batman']
+
+let arr = 0
+
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr++
+}
+
+let score = 1
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
---
+++ 05_iterrations/03_highorderarray.js
@@ -0,0 +1,19 @@
+// High Order Array Loops
+
+// for of
+
+const arr = [1, 2, 4, 5, 7]
+
+for (const num of arr) {
+    console.log(num);
+}
+
+const str = "hello world"
+
+for (const greet of str) {
+    console.log(`Each character is ${greet}`);
+}
+
+// Maps
+
+const map = new Map()
